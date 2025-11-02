f442a166255ae33fd7909bf97d7bbd5022dd36c8
matsudai-dev <237514799+matsudai-dev@users.noreply.github.com>
2025-11-02 17:56:42 +0900

    fix: update comment to clarify lint ignore for type usage

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,9 +1,10 @@
+// biome-ignore-all lint: I want to ignore the warning that says "Don't use '{}' as a type." I expect to set some kind of key in the future.
+
 interface ViteTypeOptions {
 	strictImportMetaEnv: unknown;
 }
 
-interface ImportMetaEnv {
-}
+interface ImportMetaEnv {}
 
 interface ImportMeta {
 	readonly env: ImportMetaEnv;
